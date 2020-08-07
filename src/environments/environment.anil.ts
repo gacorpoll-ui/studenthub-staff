10181452aabcc1c1873d860d8cd69454a7eafa98
Anil Kumar <anilkumar.dhiman1@gmail.com>
2020-08-07 14:51:06 +0530

    [Finishes #174088352] file upload / download and delete by admin is working now also staff can too view this file and download that

--- src/environments/environment.anil.ts
+++ src/environments/environment.anil.ts
@@ -5,8 +5,7 @@ export const environment = {
   apiEndpoint: 'http://localhost/bawes/studenthub/studenthub/staff/web/v1',
   algoliaCandidateIndex: 'anil_candidate_public',
   algoliaCacheDuration: 5 * 60 * 1000, // 5 min in millisecond
-  permanentBucketUrl: 'https://studenthub-uploads-dev-server.s3.amazonaws.com/photos/',
-  urlResume: 'https://studenthub-uploads-dev-server.s3.amazonaws.com/candidate-resume/',
+  permanentBucketUrl: 'https://studenthub-uploads-dev-server.s3.amazonaws.com/',
   cloudinaryUrl: 'https://res.cloudinary.com/studenthub/image/upload/c_thumb,w_200,h_200,g_face/v1596525812/candidate-photo/',
   environmentName: 'Anil Local Machine',
   serviceWorker: false
